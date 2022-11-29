36d567f0f5f7e3ebcf5ea1f5b3a8c17c2f2eacfc
= <sparsh.patel@kcl.ac.uk>
2022-11-29 11:39:56 +0000

    Add Thing form and show on webpage

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,15 @@
 """Forms of the project."""
 
+from .models import Thing
+from django import forms
+
 # Create your forms here.
+class ThingForm(forms.ModelForm):
+    class Meta:
+        model = Thing
+        fields = ['name', 'description', 'quantity']
+        widgets = {'description': forms.Textarea()}
+
+    # name = forms.CharField(label = 'Name', max_length=35)
+    # description = forms.CharField(label = 'Description', widget = forms.Textarea())
+    # quantity = forms.IntegerField(label = 'Quantity')
